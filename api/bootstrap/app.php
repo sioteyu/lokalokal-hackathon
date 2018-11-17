d4683d27ebcae27b0d08714880dab742e968519e
Jasper Gabriel <jasper.d.gabriel@gmail.com>
2018-11-17 21:04:59 +0800

    add cors middleware

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -67,6 +67,10 @@ $app->singleton(
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->routeMiddleware([
+    'cors' => App\Http\Middleware\CorsMiddleware::class,
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
